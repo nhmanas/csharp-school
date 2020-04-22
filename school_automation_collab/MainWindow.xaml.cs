107a9e2a1c9d35eb2cc20ebfe4783ec020470174
nhmanas <nhmanas003@gmail.com>
2020-04-22 19:20:27 +0300

    items renamed so backend staff can do better

--- school_automation_collab/MainWindow.xaml.cs
+++ school_automation_collab/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace School_Automation_Collab
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
