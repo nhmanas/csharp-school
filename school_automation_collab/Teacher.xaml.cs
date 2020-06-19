355fadaec864d048c77d35554d38b4a72703b15f
Emre Özincegedik <emreozincegedik@gmail.com>
2020-06-19 15:56:40 +0300

    student grading finished

--- school_automation_collab/Teacher.xaml.cs
+++ school_automation_collab/Teacher.xaml.cs
@@ -73,7 +73,7 @@ namespace School_Automation_Collab
             foreach (DataRow item in courseList.Rows)
             {
                 ComboboxItem comboItem = new ComboboxItem();
-                comboItem.Text = item["name"].ToString();
+                comboItem.Text = item["name"].ToString() + " ("+ item["code"].ToString()+")";
                 comboItem.Value = item["id"];
                 selectcourseCombo.Items.Add(comboItem);
                 switch (item["day"].ToString().ToLower())
@@ -126,7 +126,6 @@ namespace School_Automation_Collab
             new WarningWindow(MainWindow.colorOK, "OK", "Logout successfull", new MainWindow());
             this.Close();
         }
-
         private void selectcourseCombo_changed(object sender, EventArgs e)
         {
             
@@ -134,12 +133,102 @@ namespace School_Automation_Collab
 
         private void courseStudents_Clicked(object sender, RoutedEventArgs e)
         {
-            /*if (selectcourseCombo.SelectedIndex==0)
+            if (selectcourseCombo.SelectedIndex==0)
             {
-                new WarningWindow("")
+                new WarningWindow(MainWindow.colorWarning, "Cant list", "Please select a course first").Show();
+                return;
             }
-            string courseID = (selectcourseCombo.SelectedItem as ComboboxItem).Value.ToString();
-            var query = "select * from notes where "*/
+            if (!refresh())
+            {
+                return;
+            }
+            
+        }
+            private bool refresh()
+        {
+            string course_id = (selectcourseCombo.SelectedItem as ComboboxItem).Value.ToString();
+
+            var query = @"select 
+
+                        notes.id,
+                        access.user_id as 'Student Number',
+                        access.name as 'Student Name',
+                        access.surname as 'Student Surname',
+                        notes.note1 as 'Midterm',
+                        notes.note2 as 'Final'
+
+                        from notes join access on access.user_id=notes.student_id 
+
+                        where notes.course_id=@course_id";
+            var lstParams = new List<cmdParameterType> {
+                new cmdParameterType("@course_id",course_id)
+                };
+            var check = Database.query(query, lstParams);
+            if (check == null)
+            {
+                new WarningWindow(MainWindow.colorError, "DB Error", "Couldn't get students").Show();
+                return false;
+            }
+            if (check.Rows.Count == 0)
+            {
+                new WarningWindow(MainWindow.colorWarning, "No students", "You don't have students in this class").Show();
+            }
+            gradestudentsGrid.DataContext = check.DefaultView;
+            return true;
+        }
+
+        private void gradestudentsGrid_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (gradestudentsGrid.SelectedItems.Count==0)
+            {
+                return;
+            }
+            DataRowView selectedRow = gradestudentsGrid.SelectedItem as DataRowView;
+            midtermgradeBox.Text = selectedRow["Midterm"].ToString();
+            finalgradeBox.Text = selectedRow["Final"].ToString();
+        }
+
+        private void update_Notes(object sender, RoutedEventArgs e)
+        {
+            if (gradestudentsGrid.SelectedItems.Count !=1)
+            {
+                new WarningWindow(MainWindow.colorWarning, "Wrong selection", "Select only one student").Show();
+                return;
+            }
+
+
+            int midterm;
+            int final;
+            if ((!int.TryParse(midtermgradeBox.Text,out midterm) && midtermgradeBox.Text!="") || (!int.TryParse(finalgradeBox.Text, out final) && finalgradeBox.Text!=""))
+            {
+                new WarningWindow(MainWindow.colorWarning, "Wrong Input", "Enter number value").Show();
+                return;
+            }
+            if (midterm<0 || midterm>100 || final<0 || final>100)
+            {
+                new WarningWindow(MainWindow.colorWarning, "Wrong Input", "Midterm and final must be between 0-100").Show();
+                return;
+            }
+            DataRowView selectedRow = gradestudentsGrid.SelectedItem as DataRowView;
+            var query="update notes set note1=@note1, note2=@note2 where id=@id";
+            var lstParams = new List<cmdParameterType>
+            {
+                new cmdParameterType("@note1",midtermgradeBox.Text==""?null:midtermgradeBox.Text),
+                new cmdParameterType("@note2",finalgradeBox.Text==""?null:finalgradeBox.Text),
+                new cmdParameterType("@id",selectedRow["id"].ToString())
+
+            };
+            var check = Database.query(query, lstParams);
+            if (check == null)
+            {
+                new WarningWindow(MainWindow.colorError, "DB Error", "Couldn't update notes").Show();
+                return;
+            }
+            if (!refresh())
+            {
+                return;
+            }
+            new WarningWindow(MainWindow.colorOK, "Success", "Note Updated").Show();
         }
     }
 }
