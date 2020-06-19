8ea5aa54cb9396c0ca322ae0019e16df2dfa8d43
Emre Özincegedik <emreozincegedik@gmail.com>
2020-06-19 16:46:05 +0300

    student class schedule + note checking

--- school_automation_collab/Student.xaml.cs
+++ school_automation_collab/Student.xaml.cs
@@ -64,6 +64,71 @@ namespace School_Automation_Collab
             
             yearLabel.Content = year;
 
+
+            query = @"select 
+
+                    courses.code,
+                    courses.name,
+                    access.name as 'Teacher Name',
+                    courses.start_end,
+                    courses.day,
+                    notes.note1,
+                    notes.note2
+                    from courses join notes on courses.id = notes.course_id join instructors on instructors.id=courses.instructor_id join access on access.user_id = instructors.number
+                    where notes.student_id=@id";
+            var lstParams = new List<cmdParameterType> { new cmdParameterType("@id", id) };
+            var check = Database.query(query, lstParams);
+            if (check == null)
+            {
+                new WarningWindow().Show();
+                this.Close();
+            }
+            var courseList = check;
+            foreach (DataRow item in courseList.Rows)
+            {
+                
+                switch (item["day"].ToString().ToLower())
+                {
+                    case "monday":
+                        Teacher.class_morning_afternoon((int)item["start_end"], mon1, mon2, item["code"].ToString() + "\n" + item["name"].ToString());
+                        break;
+                    case "tuesday":
+                        Teacher.class_morning_afternoon((int)item["start_end"], tue1, tue2, item["code"].ToString() + "\n" + item["name"].ToString());
+                        break;
+                    case "wednesday":
+                        Teacher.class_morning_afternoon((int)item["start_end"], wed1, wed2, item["code"].ToString() + "\n" + item["name"].ToString());
+                        break;
+                    case "thursday":
+                        Teacher.class_morning_afternoon((int)item["start_end"], thu1, thu2, item["code"].ToString() + "\n" + item["name"].ToString());
+                        break;
+                    case "friday":
+                        Teacher.class_morning_afternoon((int)item["start_end"], fri1, fri2, item["code"].ToString() + "\n" + item["name"].ToString());
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+            DataTable dtCloned = courseList.Clone();
+            dtCloned.Columns["start_end"].DataType = typeof(string);
+            
+            foreach (DataRow item in courseList.Rows)
+            {
+                
+                dtCloned.ImportRow(item);
+            }
+            foreach (DataRow item in dtCloned.Rows)
+            {
+
+                item["start_end"] = item["start_end"].ToString() == "1" ? "9:00-12:00" : "13:00-16:00";
+            }
+            dtCloned.Columns["start_end"].ColumnName = "Hours";
+            dtCloned.Columns["note1"].ColumnName = "Midterm";
+            dtCloned.Columns["note2"].ColumnName = "Final";
+
+
+            examresultsGrid.DataContext = dtCloned.DefaultView;
+
         }
 
         private void loginButton_Click(object sender, RoutedEventArgs e)
